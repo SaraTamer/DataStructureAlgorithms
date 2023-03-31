1fd06eeb883c8635c7ebb36f1737e3a30821d918
SaraTamer <sasooelbihery@gmail.com>
2023-03-31 16:28:25 +0200

    Stack and SingleLinkedList

---
+++ SingleLinkedList.h
@@ -0,0 +1,110 @@
+
+#ifndef DATASTRUCTUREALGORITHMS_SINGLELINKEDLIST_H
+#define DATASTRUCTUREALGORITHMS_SINGLELINKEDLIST_H
+
+
+#include <iostream>
+
+#define ll long long
+
+using namespace std;
+
+
+
+template <class T>
+class SingleLinkedList{
+private:
+    struct Node{
+        Node* next;
+        T value;
+        Node()
+        {
+            value = 0;
+            next = nullptr;
+        }
+    };
+    Node *head = new Node();
+    Node *tail = new Node();
+    ll sz;
+
+public:
+    SingleLinkedList():sz(0)
+    {
+        head = nullptr;
+        tail = nullptr;
+    }
+    void insertFirst(T item)
+    {
+        Node* tmp = new Node;
+        tmp->value = item;
+        if(sz == 0)
+        {
+            tail = head = tmp;
+            tmp->next = nullptr;
+        }
+        else
+        {
+            tmp->next = head;
+            head = tmp;
+        }
+        sz++;
+
+    }
+    void insertLast(T item)
+    {
+        Node* tmp = new Node;
+        tmp->value = item;
+        if(sz == 0) {
+            head = tail = tmp;
+        }
+        else {
+            tail->next = tmp;
+            tail = tmp;
+        }
+
+        tmp->next = nullptr;
+        sz++;
+    }
+    void insertAt(T item , ll index)
+    {
+        if(index > sz || index < 0)
+        {
+            cout << "Position out of scope!\n";
+            return;
+        }
+        Node* tmp = new Node;
+        tmp->value = item;
+        if(index == 0)
+        {
+            insertFirst(item);
+        }
+        else if(index == sz-1)
+        {
+            insertLast(item);
+        }
+        else
+        {
+            while(tmp->next != nullptr)
+            {
+                tmp = tmp->next;
+            }
+        }
+        sz++;
+    }
+    bool isEmpty()
+    {
+        return (head == nullptr);
+    }
+    void print()
+    {
+        Node* tmp = head;
+        for(ll i = 0; i < sz; i++)
+        {
+            cout << tmp->value << ' ';
+            tmp = tmp->next;
+        }
+    }
+};
+
+
+#endif //DATASTRUCTUREALGORITHMS_SINGLELINKEDLIST_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "Stack.h"
+#include "SingleLinkedList.h"
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SingleLinkedList<int> intList;
+    intList.insertAt(3 , 0);
+    intList.insertAt(4 , 0);
+    intList.insertAt(2 , 0);
+
+    intList.print();
     return 0;
 }
